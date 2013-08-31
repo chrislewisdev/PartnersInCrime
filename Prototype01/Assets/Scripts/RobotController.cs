1292ff1972b76e02811c180a489d72a7ba164a52
Chris <cl_myles@hotmail.com>
2013-08-31 20:26:15 +1000

    - Updated my test scene with a very basic platforming setup - Added RobotController.cs for some test platforming controls - Add some placeholder art assets

---
+++ Prototype01/Assets/Scripts/RobotController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+		
+		if (Mathf.Abs (horizontal) > 0.1f)
+		{
+			if (horizontal > 0.1f && rigidbody.velocity.x < 15f ||
+				horizontal < 0.1f && rigidbody.velocity.x > -15f)
+			{
+				rigidbody.AddForce (-Vector3.left * horizontal * 500f);
+			}
+		}
+		else if (Mathf.Abs (rigidbody.velocity.x) > 1f)
+		{
+			rigidbody.AddForce (Vector3.left * rigidbody.velocity.x * 10);
+		}
+		
+		if (Input.GetKeyDown (KeyCode.UpArrow) && OnGround ())
+		{
+			rigidbody.AddForce (Vector3.up * 1000f);
+		}
+	}
+	
+	private bool OnGround()
+	{
+		return Physics.Raycast (new Ray(transform.position, Vector3.down), 1.7f);
+	}
+}
