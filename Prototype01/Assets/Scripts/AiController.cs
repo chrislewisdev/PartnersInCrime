887df71a63ed0122838569367e1651d29b561841
simonPederick <simonpederick@gmail.com>
2013-09-03 13:21:16 +1000

    Fixed up Ai to use raycasting/colliders
    - now press a to get to robot

--- Prototype01/Assets/Scripts/AiController.cs
+++ Prototype01/Assets/Scripts/AiController.cs
@@ -26,8 +26,7 @@ public class AiController : MonoBehaviour {
 		Vector3 input = new Vector3(Input.GetAxis("Horizontal_AI"), Input.GetAxis("Vertical_AI"), 0f);
 		if (input.magnitude > 0.2f)
 		{
-			// Horrifically unoptimized but hey its a prototype :P 
-			GameObject closestGadget = snapToClosestGadget(Mathf.Atan2(input.y, input.x), getVisibleGadgets());
+			GameObject closestGadget = checkForObject(input);
 			if (closestGadget && Input.GetButtonDown("Jump_AI"))
 				jumpToGadget(closestGadget);
 			
@@ -39,7 +38,8 @@ public class AiController : MonoBehaviour {
 			if (closestGadget)
 			{
 				selectionCircle.SetActive(true);
-				selectionCircle.transform.position = closestGadget.transform.position;
+				selectionCircle.transform.parent = closestGadget.transform;
+				selectionCircle.transform.localPosition = Vector3.zero;
 			}
 			else
 			{
@@ -60,43 +60,15 @@ public class AiController : MonoBehaviour {
 		transform.position = occupiedGadget.transform.position;
 	}
 	
-	List<GameObject> getVisibleGadgets()
+	GameObject checkForObject(Vector2 direction)
 	{
-		GameObject[] gadgets = GameObject.FindGameObjectsWithTag("AI_Controllable");
-		List<GameObject> visibleObjects = new List<GameObject>();
-		
-		foreach (GameObject g in gadgets)
+		RaycastHit hit;
+		if (Physics.Raycast(new Ray(transform.position, direction), out hit))
 		{
-			if (g != occupiedGadget && !Physics.Raycast(new Ray(transform.position, g.transform.position - transform.position), Vector3.Distance(transform.position, g.transform.position)))
-				visibleObjects.Add(g);
+			if (hit.collider.gameObject.tag == "AI_Controllable" || hit.collider.gameObject.tag == "Player")
+				return hit.collider.gameObject;
 		}
 		
-		return visibleObjects;
-	}
-	
-	//Finds gadget at closest angle to inputAngle and returns the object
-	GameObject snapToClosestGadget(float inputAngle, List<GameObject> visibleGadgets)
-	{
-		float[] angles = new float[visibleGadgets.Count];
-		for (int i = 0; i < visibleGadgets.Count; ++i)
-			angles[i] = Mathf.Atan2(visibleGadgets[i].transform.position.y - transform.position.y, 
-				visibleGadgets[i].transform.position.x - transform.position.x);
-	
-		GameObject closestGadget = null;
-		float smallestAngle = float.MaxValue;
-		for (int i = 0; i < visibleGadgets.Count; ++i)
-		{
-			float deltaAngle = Mathf.Abs(Mathf.DeltaAngle(inputAngle * Mathf.Rad2Deg, angles[i] * Mathf.Rad2Deg) * Mathf.Deg2Rad);
-			if (deltaAngle < smallestAngle)
-			{
-				smallestAngle = deltaAngle;
-				closestGadget = visibleGadgets[i];
-			}
-		}
-		
-		if (smallestAngle < SELECTION_THRESHOLD)
-			return closestGadget;
-		else
-			return null;
+		return null;
 	}
 }
