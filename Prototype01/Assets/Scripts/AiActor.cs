5e2b50a662be92e9aee4cb7d6662e6ce37cd0605
simonPederick <simonpederick@gmail.com>
2013-09-10 12:50:19 +1000

    Fixed missing file bug

---
+++ Prototype01/Assets/Scripts/AiActor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AiActor : MonoBehaviour {
+
+	public GameObject occupiedGadget;
+	
+	public void jumpToGadget(GameObject gadget)
+	{
+		transform.parent = gadget.transform;
+		transform.localPosition = Vector3.zero;
+		occupiedGadget = gadget;
+	}
+	
+	void Start()
+	{
+		jumpToGadget(occupiedGadget);
+	}
+}
