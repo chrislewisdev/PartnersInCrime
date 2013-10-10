995f13bac87e5df592a0e4c033b41f7403350ed3
simonPederick <simonpederick@gmail.com>
2013-10-10 12:27:16 +1100

    - updated moving platform to move in any direction

--- Prototype01/Assets/Scripts/Props/MovingPlatform.cs
+++ Prototype01/Assets/Scripts/Props/MovingPlatform.cs
@@ -5,13 +5,18 @@ public class MovingPlatform : AiControllable
 {
 	public float riseNumberOfBlocks;
 	public float fallNumberOfBlocks;
+	public float rightNumberOfBlocks;
+	public float leftNumberOfBlocks;
 	
 	private List<CharacterController> carryingObjects = new List<CharacterController>();
 	private Vector3 lastMovement = Vector3.zero;
 	private bool updatingPositions = false;
 	private float maxHeight;
 	private float minHeight;
+	private float leftMax;
+	private float rightMax;
 	private GameObject arrows;
+	private GameObject arrowsPrefab;
 	
 	public override void activateGadget (bool triggeredByAi)
 	{
@@ -19,23 +24,30 @@ public class MovingPlatform : AiControllable
 	
 	public override void aiSendDirection (Vector2 direction)
 	{
-		lastMovement = new Vector3(0f, direction.y, 0f) / 10f;
+		lastMovement = new Vector3(direction.x, direction.y, 0f) / 10f;
+		Vector3 newPos = transform.position + lastMovement;
+			
+		if (newPos.y > maxHeight)
+			lastMovement.y -= newPos.y - maxHeight;
+		else if (newPos.y < minHeight)
+			lastMovement.y += minHeight - newPos.y;
 		
-		float newY = (transform.position + lastMovement).y;
-		if (newY > maxHeight)
-			lastMovement.y -= newY - maxHeight;
-		else if (newY < minHeight)
-			lastMovement.y += minHeight - newY;
+		if (newPos.x > rightMax)
+			lastMovement.x -= newPos.x - rightMax;
+		else if (newPos.x < leftMax)
+			lastMovement.x += leftMax - newPos.x;
 		
 		transform.position += lastMovement;
 		
-		if (lastMovement.magnitude > 0.2f)
+		if (lastMovement.magnitude > 0.02f)
 			activateConnectedGadgets();
 	}
 	
 	public override void aiArrived ()
 	{
-		arrows = Instantiate(Resources.Load("UpDownArrows") as GameObject, transform.position + new Vector3(2f, 0f, -1f), Quaternion.identity) as GameObject;	
+		arrows = Instantiate(arrowsPrefab) as GameObject;
+		arrows.transform.parent = transform;
+		arrows.transform.localPosition = new Vector3(1f, 0f, -1f);
 	}
 	
 	public override void aiLeft ()
@@ -59,7 +71,20 @@ public class MovingPlatform : AiControllable
 	{
 		maxHeight = transform.position.y + 3.2f * riseNumberOfBlocks;
 		minHeight = transform.position.y - 3.2f * fallNumberOfBlocks;
+		leftMax = transform.position.x - 3.2f * leftNumberOfBlocks;
+		rightMax = transform.position.x + 3.2f * rightNumberOfBlocks;
 		arrows = null;
+		if (riseNumberOfBlocks != 0 || fallNumberOfBlocks != 0)
+		{
+			if (leftNumberOfBlocks == 0 && rightNumberOfBlocks == 0)
+				arrowsPrefab = Resources.Load("UpDownArrows") as GameObject;
+			else
+				arrowsPrefab = Resources.Load("AllDirectionArrows") as GameObject;
+		}
+		else if (leftNumberOfBlocks != 0 || rightNumberOfBlocks != 0)
+			arrowsPrefab = Resources.Load("LeftRightArrows") as GameObject;
+		else
+			Debug.LogError("Moving platform has no movement set");
 	}
 	
 	void LateUpdate()
