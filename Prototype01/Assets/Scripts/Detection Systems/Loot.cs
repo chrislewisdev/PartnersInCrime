5597c516f4e10bf0eee545fb159e4e4c1da8ee32
simonPederick <simonpederick@gmail.com>
2013-11-03 01:15:35 +1100

    - added loot item that when touched triggers a success screen and returns to the menu

---
+++ Prototype01/Assets/Scripts/Detection Systems/Loot.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loot : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.collider.gameObject == GameManager.gameManager.Robot.gameObject)
+		{
+			Instantiate(Resources.Load("Success") as GameObject);
+			Destroy(gameObject);
+		}
+	}
+}
